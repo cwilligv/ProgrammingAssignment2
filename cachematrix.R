cb9685727ae37cdac9d770b8f6e3cfaeccd18dfa
Christian Willig <cwillig@gmail.com>
2015-05-25 04:20:16 +1000

    Initial File

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,12 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
